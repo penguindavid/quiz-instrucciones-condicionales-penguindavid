ddf0b9bd67dc27b50798234ca0b288d3ec18d263
penguindavid <davidmacias@colegioguanenta.edu.co>
2025-02-13 16:08:21 -0500

    se agrego archivo tipo 2

---
+++ programa.py
@@ -0,0 +1,38 @@
+# input
+
+import math
+
+print(".......................")
+a=input("digite el valor de a ")
+b=input("digite el valor de b ")
+c=input("digite el valor de c ")
+print(".......................")
+
+# procesing
+
+condición = True
+((a)+(b)>(c))
+((b)+(c)>(a))
+((c)+(a)>(b))
+
+condición = False
+((a)+(b)<(c))
+((b)+(c)<(a))
+((c)+(a)<(b))
+
+
+
+if condición:
+    print("Los datos son equivalentes a los lados de un triangulo")
+
+else:
+     print("Los datos no son equivalentes a los lados de un triángulo")
+
+
+
+
+
+
+
+
+
